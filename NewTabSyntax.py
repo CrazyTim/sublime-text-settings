06978da49c09cbf6215a782554f068ecf6d02b43
CrazyTim <impdeveloper@gmail.com>
2020-02-12 23:26:12 +1100

    Replace `Markdown Extended` package with `MarkdownEditing` - Markdown Extended was printing errors in the console. - MarkdownEditing has more features and looks nicer. - Default new tab to MarkdownEditing syntax.

--- NewTabSyntax.py
+++ NewTabSyntax.py
@@ -3,4 +3,4 @@ import sublime_plugin
 
 class NewTabsAreMarkdown(sublime_plugin.EventListener):
    def on_new(self, view):
-      view.set_syntax_file('Packages/Markdown Extended/Syntaxes/Markdown Extended.sublime-syntax')
+      view.set_syntax_file('Packages/MarkdownEditing/Markdown.sublime-syntax')
